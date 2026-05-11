72c6e5e686db3b57de3b5fe4cea896fa8e10581b
David Truong <david.truong@captiveaire.com>
2026-05-11 15:38:02 -0400

    add tmp folder

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DB_FILE = path.join(__dirname, 'tmp', 'data.db');
+// const DB_FILE = path.join(__dirname, 'tmp', 'data.db');
+const DB_FILE = '/tmp/data.db';
 const TASK_ALERT_TO = process.env.TASK_ALERT_TO;
+
 
 if (!fs.existsSync(DB_FILE)) {
   fs.writeFileSync(DB_FILE, '');
